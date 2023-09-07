ee838552912e9f25324b71cb53cfe0f32cd5a2aa
ktibo <ktibo@mail.ru>
2023-09-07 15:55:41 +1000

    test5

--- src/Main.java
+++ src/Main.java
@@ -24,8 +24,8 @@ public class Main {
              DataInputStream dataIn = new DataInputStream(new FileInputStream("C:\\test\\des.txt"));
         ) {
             //dataOut.writeInt(1234);
-            int ssss = dataIn.readInt();
-            System.out.println(ssss);
+            int sssss = dataIn.readInt();
+            System.out.println(sssss);
             System.out.println("OK");
 
         }catch (Exception e){
