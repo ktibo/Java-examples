d5246a17a99899a02350777fe0f435cd7119d8af
ktibo <ktibo@mail.ru>
2023-10-08 20:06:50 +1000

    Extract i/o methods

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ public class Main {
 
     public static void main(String... args) {
 
-        System.out.println("Insert number:\n" +
-                "0: RandomAccessFile");
-
-        System.out.println("-1: Quit");
-
         while (true) {
+
+            System.out.println("Enter a number.\n" +
+                    "1: randomAccessFile\n"+
+                    "2: bufferredReader"
+            );
+
+            System.out.println("0: Quit");
 
             int option;
             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -19,7 +21,7 @@ public class Main {
                 res = input.readLine();
                 option = Integer.parseInt(res);
             } catch (NumberFormatException e) {
-                System.out.println("\""+res+"\" is not a number! Try again.");
+                System.out.println("\""+res+"\" is not a number!");
                 continue;
             } catch (Exception e) {
                 e.printStackTrace();
@@ -27,29 +29,66 @@ public class Main {
             }
 
             switch (option) {
-                case 0:
-                    try (RandomAccessFile raf = new RandomAccessFile("C:\\test\\src.txt", "rw")) {
-                        //raf.seek(100);
-                        raf.writeBoolean(true);
-                        raf.writeBoolean(false);
-                        raf.writeChar('f');
-
-                        raf.seek(1);
-                        //boolean b = raf.readBoolean();
-                        char ch = raf.readChar();
-
-                        System.out.println(ch);
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                case 0: return;
+                case 1:
+                    randomAccessFile();
                     break;
-                case -1: return;
+                case 2:
+                    bufferredReader();
+                    break;
                 default:
                     System.out.println("Option " + option + " isn't specified!");
             }
+
         }
 
     }
 
+
+
+    private static void randomAccessFile() {
+
+        String fileName;
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter file name: ");
+            fileName = input.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try (RandomAccessFile raf = new RandomAccessFile("C:\\test\\"+fileName+".txt", "rw")) {
+            //raf.seek(100);
+            raf.writeBoolean(true);
+            raf.writeBoolean(false);
+            raf.writeChar('f');
+
+            raf.seek(2);
+            //boolean b = raf.readBoolean();
+            char ch = raf.readChar();
+
+            System.out.println(ch);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    private static void bufferredReader() {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Type some symbols here: ");
+
+        try {
+            //char s = (char)bf.read();
+            //System.out.println(s);
+            String str = bf.readLine();
+            System.out.println("Your string: "+str);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
